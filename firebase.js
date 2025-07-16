4ef0566ae05f64034e53c3e8c589b5b460ef91bb
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-07-16 19:18:54 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,2 +1,15 @@
+// Substitua com suas credenciais do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, onSnapshot, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-<script type=" ">
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
