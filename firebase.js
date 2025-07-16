c7d9ea1385fba93c43abcda0a06e4b8232065364
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-07-16 19:23:07 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
-// Substitua com suas credenciais do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, onSnapshot, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
+// firebase.js
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBHIc2E4XwRO5FXo4uHlTQVRArOis73MjE",
+  authDomain: "projeto-deus-yato-928-sk.firebaseapp.com",
+  databaseURL: "https://projeto-deus-yato-928-sk-default-rtdb.firebaseio.com",
+  projectId: "projeto-deus-yato-928-sk",
+  storageBucket: "projeto-deus-yato-928-sk.appspot.com",
+  messagingSenderId: "790408726854",
+  appId: "1:790408726854:android:e2f0de7b7d5dba96b0fd47"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
